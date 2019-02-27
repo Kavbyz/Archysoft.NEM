9982f86c2d2444aca754cc029bb64f88a0ca19e1
ValeriiR <valeriirohachko@gmail.com>
2019-02-27 02:14:56 +0200

    Change Transaction Service

--- Archysoft.NEM.Domain.Model/Services/Concrete/TransactionService.cs
+++ Archysoft.NEM.Domain.Model/Services/Concrete/TransactionService.cs
@@ -27,43 +27,33 @@ namespace Archysoft.NEM.Domain.Model.Services.Concrete
 
         string host = "http://23.228.67.85:7890";
         string host2 = "http://104.128.226.60:7890";
+
+        private readonly string privateKey = "6ea3fd5f2cf4fbeb54cd96a48d11cd2ff0b4106472c6a97c7e4e5736243cb2db";
+        private readonly string recipientAddress = "TACOPE-XRLZTU-WBQA3U-XV66R4-55L76E-NWK6OY-ITBJ";
+
         public async Task<TransactionResponse> Send(object data)
         {
-            var cosignatory = KeyPair.CreateFromPrivateKey("8db858dcc8e2827074498204b3829154ec4c4f24d13738d3f501003b518ef256");
-            var secondCosig = KeyPair.CreateFromPrivateKey("cfe47dd9801a5d4fe37183e8f6ca49fff532a2fe6fe099436df93b3d62fe17d5");
-            var multisigAccount = PublicAccount.CreateFromPublicKey("29c4a4aa674953749053c8a35399b37b713dedd5d002cb29b3331e56ff1ea65a", NetworkType.Types.TEST_NET);
-            var recipient = new Account("E45030D2A22D97FDC4C78923C4BBF7602BBAC3B018FFAD2ED278FB49CD6F218C", NetworkType.Types.TEST_NET);
+            //var cosignatory = KeyPair.CreateFromPrivateKey("8db858dcc8e2827074498204b3829154ec4c4f24d13738d3f501003b518ef256");
+            //var secondCosig = KeyPair.CreateFromPrivateKey("cfe47dd9801a5d4fe37183e8f6ca49fff532a2fe6fe099436df93b3d62fe17d5");
+            //var multisigAccount = PublicAccount.CreateFromPublicKey("29c4a4aa674953749053c8a35399b37b713dedd5d002cb29b3331e56ff1ea65a", NetworkType.Types.TEST_NET);
+            //var recipient = new Account("E45030D2A22D97FDC4C78923C4BBF7602BBAC3B018FFAD2ED278FB49CD6F218C", NetworkType.Types.TEST_NET);
+        
 
-            var transaction = TransferTransaction.Create(
-                NetworkType.Types.TEST_NET,
-                Deadline.CreateHours(2),
-                recipient.Address,
-                new List<Mosaic> { Mosaic.CreateFromIdentifier("nem:xem", 1000000) },
-                PlainMessage.Create("hello")
-            );
+            KeyPair keyPair = KeyPair.CreateFromPrivateKey(privateKey);
 
-            var multisigTransaction = MultisigTransaction
-                .Create(NetworkType.Types.TEST_NET, Deadline.CreateHours(1), transaction)
-                .SignWith(cosignatory, multisigAccount);
-
-
-
-            TransactionResponse response = await new TransactionHttp(host).Announce(multisigTransaction);
-
-            var signatureTransaction = CosignatureTransaction.Create(
+            TransferTransaction transaction = TransferTransaction.Create(
                 NetworkType.Types.TEST_NET,
                 Deadline.CreateHours(1),
-                "59f5f7cbbdaa996b8d3c45ce814280aab3b5d322a98fe95c00ae516cf436172d",
-                multisigAccount.Address
-            ).SignWith(secondCosig);
+                Address.CreateFromEncoded(recipientAddress),
+                new List<Mosaic> { Mosaic.CreateFromIdentifier("nem:xem", 1) },
+                PlainMessage.Create(data.ToString())
+            );
 
-            TransactionResponse response2 = await new TransactionHttp(host).Announce(signatureTransaction);
+            SignedTransaction signedTransaction = transaction.SignWith(keyPair);
+          
+            TransactionResponse response = await new TransactionHttp(host).Announce(signedTransaction);
 
-            //var localVarPath = "/transaction/announce";
-            //var client = new RestClient(host);
-            //var request = new RestRequest(localVarPath, Method.POST);
-            //request.AddParameter("application/json", data, ParameterType.RequestBody);
-            //var resp = client.Execute(request);
+
 
             if (response.Status == "1")
             {
